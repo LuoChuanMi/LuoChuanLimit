1c41ab38eae011523f8b432f25a0a5dad7aa765b
小白 <3104026189@qq.com>
2022-11-07 20:55:04 +0800

    update

--- src/main/java/com/xbaimiao/luochuan/limit/LimitListener.java
+++ src/main/java/com/xbaimiao/luochuan/limit/LimitListener.java
@@ -2,11 +2,14 @@ package com.xbaimiao.luochuan.limit;
 
 import com.xbaimiao.luochuan.limit.data.BlockBreakData;
 import com.xbaimiao.luochuan.limit.data.PlayerData;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 import top.mcplugin.lib.common.subscribe.AutoListener;
+import top.mcplugin.lib.module.item.ItemUtil;
 import top.mcplugin.lib.module.lang.Lang;
 
 @AutoListener
@@ -16,6 +19,13 @@ public class LimitListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void b(BlockBreakEvent event) {
         Player player = event.getPlayer();
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
+        if (ItemUtil.isAir(itemStack)) {
+            return;
+        }
+        if (!checkItem(itemStack)) {
+            return;
+        }
         BlockBreakData blockBreakData = PlayerData.getBlockBreakData(player);
         if (blockBreakData.get(event.getBlock().getLocation()) > 64) {
             event.setCancelled(true);
@@ -23,6 +33,15 @@ public class LimitListener implements Listener {
         } else {
             blockBreakData.add(event.getBlock().getLocation());
         }
+    }
+
+    private boolean checkItem(ItemStack itemStack) {
+        for (Enchantment enchantment : itemStack.getEnchantments().keySet()) {
+            if (enchantment.toString().contains("transfuse")) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }

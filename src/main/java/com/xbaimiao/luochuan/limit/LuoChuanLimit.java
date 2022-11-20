0f0b14cea5d78d71293b6fe52c62a5f2ceb36a29
小白 <3104026189@qq.com>
2022-11-20 12:52:23 +0800

    取消限制掉落物

--- src/main/java/com/xbaimiao/luochuan/limit/LuoChuanLimit.java
+++ src/main/java/com/xbaimiao/luochuan/limit/LuoChuanLimit.java
@@ -1,12 +1,9 @@
 package com.xbaimiao.luochuan.limit;
 
-import com.xbaimiao.luochuan.limit.entity.ItemLimit;
 import top.mcplugin.lib.Plugin;
 
 @SuppressWarnings("unused")
 public class LuoChuanLimit extends Plugin {
-
-    private ItemLimit itemLimit;
 
     public LuoChuanLimit() {
         ignoreScan("shadow");
@@ -15,12 +12,9 @@ public class LuoChuanLimit extends Plugin {
     @Override
     public void enable() {
         saveDefaultConfig();
-        itemLimit = new ItemLimit(getConfig().getLong("drop-item-available-time"));
-        itemLimit.start();
     }
 
     @Override
     public void disable() {
-        itemLimit.close();
     }
 }
